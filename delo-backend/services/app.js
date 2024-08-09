a46fe199c6bda9668c7c85fda4e6e97ebf7f1e37
SankhaDas1993 <sankha.das@delostylestudio.com>
2024-08-09 10:08:25 +0530

    Update app.js
    changes in app.js

--- delo-backend/services/app.js
+++ delo-backend/services/app.js
@@ -25,7 +25,9 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 
-app.use(express.json())
+app.use(express.json({
+  limit: '4MB',
+}))
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser())
 app.use(bodyParser.json({ limit: '1000mb' }));
